f4c5b059d57a6da6e66b3d51b5931280a93044b2
Vita <4studywork@gmail.com>
2020-09-14 21:01:34 +0300

    Add files via upload

---
+++ task_3/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueueDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Вызовем метод Peek для пустой очереди:");
+            QueueDemo queue = new QueueDemo();
+            queue.Peek();
+            Console.WriteLine();
+            Console.WriteLine("Вызовем метод Dequeue для пустой очереди:");
+            queue.Dequeue();
+            Console.WriteLine();
+            Console.WriteLine("Число элементов в пустой очереди: {0}", queue._Count);
+            Console.WriteLine();
+            Console.WriteLine("Создаем и заполняем нашу очередь, вызов метода Enqueue");
+            queue.Enqueue(new Complex(5, 6));
+            queue.Enqueue(new Complex(1, 6));
+            queue.Enqueue(new Complex(2, 2));
+            queue.Enqueue(new Complex(5, 6));
+            queue.Enqueue(new Complex(4, 0));
+            queue.Enqueue(new Complex(5, 9));
+            queue.Enqueue(new Complex(7, 4));
+
+            Console.WriteLine("Число элементов в созданной очереди: {0}", queue._Count);
+            Console.WriteLine("Печать элементов созданной очереди:");
+            queue.Print();
+            Console.WriteLine();
+            Console.WriteLine("Число, находящееся в начале очереди: {0}", queue.Peek());
+            Console.WriteLine("Удалим верхушку очереди, вызов метода Dequeue");
+            queue.Dequeue();
+            Console.WriteLine();
+            Console.WriteLine("Теперь число элементов в очереди: {0}", queue._Count);
+            Console.WriteLine("Теперь число, находящееся в начале очереди: {0}", queue.Peek());
+            Console.WriteLine("Печать элементов измененной очереди:");
+            queue.Print();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ task_3/QueueDemo.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueueDemo
+{
+    class QueueDemo
+    {
+        private Node _first;
+        private Node _last;
+        public int _Count = 0;
+
+        //Создаем узел
+        #region
+        private class Node
+        {
+            public Complex Data { get; set; }
+            public Node next { get; set; }
+
+            public Node(Complex data)
+            {
+                Data = data;
+            }
+        }
+        #endregion
+        //Помещает число в очередь(в конец)
+        #region
+        public void Enqueue(Complex data)
+        {
+            _Count++;
+            Node temp = new Node(data);
+            if (_first == null)
+            {
+                _first = _last = temp;
+            }
+            else
+            {
+                _last.next = temp;
+                _last = temp;
+            }
+        }
+        #endregion
+        //Получает число из начала очереди и удаляет его
+        #region
+        public Complex Dequeue()
+        {
+            if (_first == null)
+            {
+                Console.WriteLine("Очередь пустая.");
+                return 0;
+            }
+            else
+            {
+                _Count--;
+                Node current = _first;
+                _first = current.next;
+                return _first.Data;
+            }
+        }
+        #endregion
+        //Возвращает число, находящееся в начале очереди
+        #region
+        public Complex Peek()
+        {
+            if (_first == null)
+            {
+                Console.WriteLine("Очередь пустая.");
+                return 0;
+            }
+            else
+            {
+                return _first.Data;
+            }
+        }
+        #endregion
+        //Печать очереди
+        #region
+        public void Print()
+        {
+            Node temp = _first;
+            int i = 0;
+            while (temp != null && i <= _Count)
+            {
+                i++;
+                Console.WriteLine(temp.Data);
+                temp = temp.next;
+            }
+        }
+        #endregion
+    }
+}
