b722804eef86eb902b53cce6fd8d602c42fc2f00
Vita <4studywork@gmail.com>
2020-09-14 21:40:56 +0300

    Add files via upload

---
+++ task_7/7_2/FileMonitoring.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.IO;
+
+
+namespace ConsoleApp1
+{
+    class FileMonitoring
+    {
+        private string fullFileName;
+        private string logFileName;
+        public void startMonitoring(string getFileName, string getLogFileName)
+        {
+            FileSystemWatcher watcher = new FileSystemWatcher();
+
+            fullFileName = getFileName;
+            logFileName = getLogFileName;
+            CreateLogFile(logFileName);
+
+            watcher.Path = Path.GetDirectoryName(fullFileName);
+            watcher.NotifyFilter = NotifyFilters.LastWrite | NotifyFilters.FileName | NotifyFilters.Size;
+            watcher.Filter = "*.cs";
+
+            watcher.Deleted += new FileSystemEventHandler(OnDelete);
+            watcher.Renamed += new RenamedEventHandler(OnRenamed);
+            watcher.Changed += new FileSystemEventHandler(OnChanged);
+            watcher.Created += new FileSystemEventHandler(OnCreate);
+            watcher.EnableRaisingEvents = true;
+        }
+        private void OnChanged(object source, FileSystemEventArgs e)
+        {
+            string str = DateTime.Now + "\t" + e.Name + "\t\t" + e.ChangeType.ToString().ToUpper();
+            Console.WriteLine(str);
+            WrightToLogFile(logFileName, str);
+        }
+
+        private void OnRenamed(object source, RenamedEventArgs e)
+        {
+            string str = DateTime.Now + "\t" + e.Name + "\t\t" + e.ChangeType.ToString().ToUpper();
+            Console.WriteLine(str);
+            WrightToLogFile(logFileName, str);
+        }
+
+        private void OnDelete(object source, FileSystemEventArgs e)
+        {
+            string str = DateTime.Now + "\t" + e.Name + "\t\t" + e.ChangeType.ToString().ToUpper();
+            Console.WriteLine(str);
+            WrightToLogFile(logFileName, str);
+        }
+        private void OnCreate(object source, FileSystemEventArgs e)
+        {
+            string str = DateTime.Now + "\t" + e.Name + "\t\t" + e.ChangeType.ToString().ToUpper();
+            Console.WriteLine(str);
+            WrightToLogFile(logFileName, str);
+        }
+
+        private void CreateLogFile(string FileName)
+        {
+            try
+            {
+                string tempPath = Path.GetTempPath();
+                logFileName = Path.Combine(tempPath, FileName);
+                File.AppendAllText(logFileName, "Date, Time:\t\tName:\t\t\t\tChange Type:" + Environment.NewLine);
+                File.AppendAllText(logFileName, "-------------------------------------------------------" + Environment.NewLine);
+                Console.WriteLine($"Cоздали log файл: {logFileName}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private void WrightToLogFile(string fileName, string str)
+        {
+            try
+            {
+                File.AppendAllText(fileName, str + Environment.NewLine);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ task_7/7_2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Введите путь к папке: ");
+                string fullFileName = Console.ReadLine();
+                Console.WriteLine("Введите имя log-файла: ");
+                string logFileName = Console.ReadLine();
+
+                Console.WriteLine("Нажмите Enter для выхода ...");
+                FileMonitoring fileMonitoring = new FileMonitoring();
+                fileMonitoring.startMonitoring(fullFileName, logFileName);
+
+                Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine("Нажмите Enter для выхода ...");
+                Console.ReadLine();
+            }
+        }
+    }
+}
