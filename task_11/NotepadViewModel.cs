cc4b3f90a5245bdc31959d839aeae3b96b0987e3
Vita <4studywork@gmail.com>
2020-09-16 19:06:50 +0300

    Add files via upload

---
+++ task_11/NotepadViewModel.cs
@@ -0,0 +1,92 @@
+﻿//using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Runtime.CompilerServices;
+using System.Windows.Forms;
+
+
+
+namespace Notepad
+{
+    class NotepadViewModel
+    {
+
+        public bool HaveChange { get; set; }
+        public System.Windows.Controls.TextBox Npad { get; set; }
+        public string Text { get; set; }
+        public string FileName { get; set; }
+        
+
+        public NotepadViewModel()
+        {
+            HaveChange = false;
+            InitializeCommands();
+        }
+
+        
+        public void text_Changed(object sender, EventArgs e)
+        {
+            HaveChange = true;
+        }
+
+        public RelayCommand OpenCommand { get; private set; }
+        public RelayCommand SaveCommand { get; private set; }
+        public RelayCommand CloseCommand { get; private set; }
+
+        private void InitializeCommands()
+        {
+            OpenCommand = new RelayCommand(o => OpenFile());
+            SaveCommand = new RelayCommand(o => SaveFile());
+            CloseCommand = new RelayCommand(o => CloseApp());
+            
+        }
+
+        public void OpenFile()
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                FileName = openFileDialog.FileName;
+                using (StreamReader file = new StreamReader(FileName))
+                {
+                    Npad.Text = file.ReadToEnd();
+                }
+            }
+        }
+        public void SaveFile()
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            if (FileName != "") { saveFileDialog.FileName = FileName; }
+            if (saveFileDialog.ShowDialog() == DialogResult.Cancel) { return; }
+            using (StreamWriter file = new StreamWriter (saveFileDialog.FileName))
+            {
+                
+                file.Write(Npad.Text);
+            }
+            HaveChange = false;
+        }
+        public void CloseApp()
+        {
+            if (HaveChange == true)
+            {
+                DialogResult result = MessageBox.Show("Сохранить файл?", "Закрыть", MessageBoxButtons.YesNoCancel);
+                if (result == DialogResult.Cancel) { return; }
+                if (result == DialogResult.Yes) { SaveFile(); }
+            }
+            Application.Exit();
+        }
+        public void CloseWin(object sender, EventArgs e)
+        {
+            CloseApp();
+        }
+
+    }
+}
---
+++ task_11/WrapConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace Notepad
+{
+    [ValueConversion(typeof(bool), typeof(string))]
+    class WrapConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value == null && value.GetType() != typeof(bool)) return DependencyProperty.UnsetValue;
+            if ((bool)value) return "Wrap";
+            return "NoWrap";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
